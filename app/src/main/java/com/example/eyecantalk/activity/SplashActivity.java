e960ce3d075dda5a47f414b2547604dbfb2d4e23
bskim <skdkwl900@gmail.com>
2023-09-13 00:17:41 +0900

    feat: 스플래시 액티비티 및 앱 아이콘
    앱의 디자인 로고를 보여주는 스플래시 액티비티 추가와 앱 아이콘 추가

---
+++ app/src/main/java/com/example/eyecantalk/activity/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.eyecantalk.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eyecantalk.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // 스플래시 화면을 표시한 후 메인 액티비티로 이동
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000); // 2초간 스플래시 화면을 보여줌
+    }
+}
